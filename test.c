1a9f08058df5cbf7a46bd57ae3489443a021bc19
JH Baek <zirubak@gmail.com>
2014-07-16 17:30:04 -0700

    example struct

--- test.c
+++ test.c
@@ -3,7 +3,18 @@
 
 int main()
 {
-	printf("test");
+	struct xx
+	{
+		int x;
+		char name[];
+	};
 
-   return 0;
+	struct xx *s = (struct xx*)malloc(sizeof(struct xx));
+	s->x = 10;
+	strcpy(s->name, "hi");
+
+	printf("%d\n",s->x);
+	printf("%s\n",s->name);
+
+	return 0;
 }
